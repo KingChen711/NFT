682d10425814f8c585d5215656d1bdbd631bdeb0
KingChen711 <Kingchenobama711@gmail.com>
2023-03-08 21:47:24 +0700

    create nft page, valid form 8/3/2023

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function Button({
-  children, className, variant, handleClick,
-}) {
+function Button({ children, className, variant, handleClick, type }) {
   return (
     <button
       onClick={handleClick}
-      type="button"
+      type={type || "button"}
       className={`
       button ${
-                variant === 'outlined' ? 'button__outlined' : 'button__contained'
-            } ${className} 
+        variant === "outlined" ? "button__outlined" : "button__contained"
+      } ${className} 
       `}
     >
       {children}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
--- pages/create-nft.js
+++ pages/create-nft.js
@@ -5,15 +5,111 @@ import Image from "next/image";
 import images from "../assets";
 import { useTheme } from "next-themes";
 import { Button } from "../components";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function CreateNft() {
-  const { theme } = useTheme();
   const [fileUrl, setFileUrl] = useState(null);
 
   const onDrop = useCallback(() => {
     // upload image to the ipfs
   }, []);
 
+  return (
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-3/5 md:w-full">
+        <div className="font-semibold text-3xl dark:text-white text-nft-black-1 font-poppins">
+          Create new NFT
+        </div>
+        <div className="mt-16">
+          <ImageField fileUrl={fileUrl} onDrop={onDrop} />
+          <FormNFT />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateNft;
+
+function FormNFT() {
+  const initialValues = {
+    name: "",
+    description: "",
+    price: "",
+  };
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Required"),
+    description: Yup.string().required("Required"),
+    price: Yup.number().required("Required"),
+  });
+  const onSubmit = (value) => {
+    console.log(value);
+  };
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div className="flex items-center mb-5">
+          <div className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
+            Name
+          </div>
+          <div className="text-red-500 ml-3">
+            <ErrorMessage name="name" />
+          </div>
+        </div>
+        <Field
+          name="name"
+          type="text"
+          placeholder="Item Name"
+          className="py-4 px-7 rounded-lg w-full dark:bg-nft-black-1 dark:border-none outline-none border border-nft-gray-1"
+        />
+
+        <div className="flex items-center mb-5 mt-14">
+          <div className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
+            Description
+          </div>
+          <div className="text-red-500 ml-3">
+            <ErrorMessage name="description" />
+          </div>
+        </div>
+        <Field
+          component="textarea"
+          name="description"
+          rows={5}
+          placeholder="Description of your item"
+          className="py-4 px-7 rounded-lg w-full dark:bg-nft-black-1 dark:border-none outline-none border border-nft-gray-1"
+        />
+        <div className="flex items-center mb-5 mt-14">
+          <div className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
+            Price
+          </div>
+          <div className="text-red-500 ml-3">
+            <ErrorMessage name="price" />
+          </div>
+        </div>
+        <div className="flex items-center dark:bg-nft-black-1 rounded-lg pr-7 border border-nft-gray-1 dark:border-none mb-14">
+          <Field
+            type="number"
+            name="price"
+            placeholder="Enter price"
+            className="w-0 flex-1 bg-inherit dark:border-none outline-none py-4 px-7"
+          />
+          <div>ETH</div>
+        </div>
+        <Button type="submit" variant="contained" className="float-right">
+          Create Item
+        </Button>
+      </Form>
+    </Formik>
+  );
+}
+
+function ImageField({ fileUrl, onDrop }) {
+  const { theme } = useTheme();
   const {
     getRootProps,
     getInputProps,
@@ -25,7 +121,6 @@ function CreateNft() {
     accept: "image/*",
     maxSize: 5000000,
   });
-
   const fileStyle = useMemo(
     () =>
       `
@@ -36,54 +131,44 @@ function CreateNft() {
     `,
     []
   );
-
   return (
-    <div className="flex justify-center sm:px-4 p-12">
-      <div className="w-3/5 md:w-full">
-        <div className="font-semibold text-3xl dark:text-white text-nft-black-1 font-poppins">
-          Create new NFT
-        </div>
-        <div className="mt-16">
-          <p className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
-            Upload File
-          </p>
-          <div className="mt-4">
-            <div {...getRootProps()} className={fileStyle}>
-              <input {...getInputProps()} />
-              <div className="flexCenter flex-col text-center">
-                <p className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
-                  JPG, PNG, GIF, SVG, WEBM, MP3, MP4. Max 100mb
-                </p>
-                <div className="my-12 w-full flex justify-center">
-                  <Image
-                    src={images.upload}
-                    width={100}
-                    height={100}
-                    alt="file_upload"
-                    className={theme === "light" && "invert filter"}
-                  />
-                </div>
-
-                <p className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
-                  Drag and Drop File
-                </p>
-                <p className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
-                  or Browse media on your device
-                </p>
-              </div>
+    <>
+      <p className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
+        Upload File
+      </p>
+      <div className="mt-4 mb-14">
+        <div {...getRootProps()} className={fileStyle}>
+          <input {...getInputProps()} />
+          <div className="flexCenter flex-col text-center">
+            <p className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
+              JPG, PNG, GIF, SVG, WEBM, MP3, MP4. Max 100mb
+            </p>
+            <div className="my-12 w-full flex justify-center">
+              <Image
+                src={images.upload}
+                width={100}
+                height={100}
+                alt="file_upload"
+                className={theme === "light" && "invert filter"}
+              />
             </div>
-            {fileUrl && (
-              <aside>
-                <div>
-                  <img src={fileUrl} alt="asset_file" />
-                </div>
-              </aside>
-            )}
+
+            <p className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
+              Drag and Drop File
+            </p>
+            <p className="dark:text-white text-nft-black-1 text-xl font-semibold font-poppins">
+              or Browse media on your device
+            </p>
           </div>
         </div>
+        {fileUrl && (
+          <aside>
+            <div>
+              <img src={fileUrl} alt="asset_file" />
+            </div>
+          </aside>
+        )}
       </div>
-    </div>
+    </>
   );
 }
-
-export default CreateNft;
